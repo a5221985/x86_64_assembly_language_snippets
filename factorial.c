8501c3c6ea63bc0cd9a868c551f05f9db965491c
a5221985 <abdm03142014@gmail.com>
2019-12-30 10:06:48 +0530

    Modified to print more factorials

--- factorial.c
+++ factorial.c
@@ -4,11 +4,7 @@
 int64_t factorial(int64_t);
 
 int main() {
-    printf("%d! = %ld\n", 0, factorial(0));
-    printf("%d! = %ld\n", 1, factorial(1));
-    printf("%d! = %ld\n", 2, factorial(2));
-    printf("%d! = %ld\n", 3, factorial(3));
-    printf("%d! = %ld\n", 4, factorial(4));
-    printf("%d! = %ld\n", 5, factorial(5));
+    for (int64_t i = 0; i < 20; i++)
+        printf("%ld! = %ld\n", i, factorial(i));
     return 0;
 }
