14ccbeddaa58d5ed7ba0dbf3f7eeac726be22fdb
a5221985 <abdm03142014@gmail.com>
2019-12-31 19:27:10 +0530

    New code

--- gcd.asm
+++ gcd.asm
@@ -1,26 +1,29 @@
-	global	gcd
-	section	.text
+; ------------------------------------------------------
+; Function to calculate GCD of two numbers
+; condition: a < b
+; Prototype:
+;	uint64_t gcd(uin64_t a, uint64_t b);
+; ------------------------------------------------------
+
+	global	gcd		; expose gcd function
+	section	.text		; code section
 gcd:
 	cmp	rdi,	0	; a == 0?
 	jne	calculate	; if not, calculate gcd
 	mov	rax,	rsi	; if true, return b
 	ret
 calculate:
-	push	rdi
-	push	rsi
-	call modulo
-	pop	rsi
-	pop	rdi
-	mov	r8,	rdi	; t <- a
-	mov	rdi,	rax	; a <- b % a
-	mov	rsi,	r8	; b <- a
-	call	gcd
+	mov	r8,	rdi	; t1 <- a
+	mov	r9,	rsi	; t2 <- b
+	call modulo		; m <- t2 % t1
+	mov	rsi,	rdi	; b <- a
+	mov	rdi,	r9	; a <- b % a
+	call	gcd		; recursive call
 	ret
 modulo:
-	cmp	rsi,	rdi
-	jl	done
-	sub	rsi,	rdi
-	jmp	modulo
-done:
-	mov	rax,	rsi
+	cmp	r9,	r8
+	jl	done		; done if t2 < t1
+	sub	r9,	r8	; t2 <- t2 - t1
+	jmp	modulo		; loop until t2 < t1
+done:				; done calculating
 	ret
